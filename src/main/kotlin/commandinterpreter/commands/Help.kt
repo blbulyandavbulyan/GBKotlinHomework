f8a077b3e817ba71306ad6f9a0574b4997285b05
David Blbulyan <blbulyandavbulyan@gmail.com>
2024-03-11 12:49:18 +0400

    added implementation for Help#execute

--- src/main/kotlin/commandinterpreter/commands/Help.kt
+++ src/main/kotlin/commandinterpreter/commands/Help.kt
@@ -4,6 +4,10 @@ import commandinterpreter.PeopleStorage
 
 class Help(cmd: String): AbstractCommand(Regex("help"), cmd) {
     override fun execute(peopleStorage: PeopleStorage) {
-        TODO("Not implemented yet")
+        println("Справка по командам")
+        println("Добавление телефона: add <Имя> phone <Номер телефона>")
+        println("Добавление email: add <Имя> email <Адрес электронной почты>")
+        println("Показать последнего добавленного человека: show")
+        println("Выход: exit")
     }
 }
