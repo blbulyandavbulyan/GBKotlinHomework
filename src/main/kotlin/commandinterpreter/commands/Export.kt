3be581c40bf790c105b3673e2092cbe8a3af1038
David Blbulyan <blbulyandavbulyan@gmail.com>
2024-03-16 15:05:47 +0400

    added Export command

---
+++ src/main/kotlin/commandinterpreter/commands/Export.kt
@@ -0,0 +1,27 @@
+package commandinterpreter.commands
+
+import commandinterpreter.PeopleStorage
+import commandinterpreter.annotations.UsedViaReflection
+import commandinterpreter.json.JSON
+import commandinterpreter.json.JSON.`object`
+import commandinterpreter.throwIfNull
+import java.io.File
+
+@UsedViaReflection
+class Export(cmd: String) : AbstractCommand(Regex("export (.+)"), cmd) {
+    override fun execute(peopleStorage: PeopleStorage) {
+        val matchGroupCollection = regex.find(cmd)?.groups ?: throw RuntimeException("Command is not valid!")
+        val path = matchGroupCollection[1]?.value.throwIfNull()
+        File(path).writeText(JSON.array {
+                for (person in peopleStorage) {
+                    item {
+                        `object` {
+                            "emails" with person.emails
+                            "phones" with person.phones
+                            "name" with person.name
+                        }
+                    }
+                }
+            }.toString())
+    }
+}
